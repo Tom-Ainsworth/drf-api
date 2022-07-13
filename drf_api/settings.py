7505b6531bd8582139622afa9b7302a1de4f4b8e
Tom Ainsworth <tomainsworth94@gmail.com>
2022-07-13 11:36:46 +0100

    Add profile serializer and list view

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "cloudinary_storage",
     "django.contrib.staticfiles",
     "cloudinary",
+    "rest_framework",
     "drf_api.profiles",
 ]
 
