574bc14f36a9f3204c4b9424364f1e3f353f96c0
Tom Ainsworth <tomainsworth94@gmail.com>
2022-07-18 18:25:39 +0100

    Add queryset annotations to display followers/following and posts counts

--- drf_api/profiles/views.py
+++ drf_api/profiles/views.py
@@ -1,7 +1,9 @@
-from rest_framework import generics
+from math import dist
+from rest_framework import generics, filters
 from .models import Profile
 from .serializers import ProfileSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
+from django.db.models import Count
 
 
 class ProfileList(generics.ListAPIView):
@@ -11,7 +13,19 @@ class ProfileList(generics.ListAPIView):
     """
 
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count("owner__post", distinct=True),
+        followers_count=Count("owner__followed", distinct=True),
+        following_count=Count("owner__following", distinct=True),
+    ).order_by("-created_at")
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = [
+        "posts_count",
+        "followers_count",
+        "following_count",
+        "owner__following__created_at",
+        "owner__followed__created_at",
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -21,4 +35,8 @@ class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
 
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count("owner__post", distinct=True),
+        followers_count=Count("owner__followed", distinct=True),
+        following_count=Count("owner__following", distinct=True),
+    ).order_by("-created_at")
