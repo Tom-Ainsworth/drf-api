e14a044273af7a62c8720a0124151bbee3ac317b
Tom Ainsworth <tomainsworth94@gmail.com>
2022-07-19 12:17:05 +0100

    Fix: Add logout view as cookies were not being passed to the original view, stopping users from logging out

--- drf_api/views.py
+++ drf_api/views.py
@@ -1,3 +1,9 @@
+from .settings import (
+    JWT_AUTH_COOKIE,
+    JWT_AUTH_REFRESH_COOKIE,
+    JWT_AUTH_SAMESITE,
+    JWT_AUTH_SECURE,
+)
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -5,3 +11,27 @@ from rest_framework.response import Response
 @api_view()
 def root_route(request):
     return Response({"message": "Welcome to my Django Rest Framework API"})
+
+
+@api_view(["POST"])
+def logout_route(request):
+    response = Response()
+    response.set_cookie(
+        key=JWT_AUTH_COOKIE,
+        value="",
+        httponly=True,
+        expires="Thu, 01 Jan 1970 00:00:00 GMT",
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    response.set_cookie(
+        key=JWT_AUTH_REFRESH_COOKIE,
+        value="",
+        httponly=True,
+        expires="Thu, 01 Jan 1970 00:00:00 GMT",
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    return response
