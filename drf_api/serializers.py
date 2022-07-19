1257a5470ddb49cd7a9e37a586e9780e4cd5f915
Tom Ainsworth <tomainsworth94@gmail.com>
2022-07-19 11:45:05 +0100

    Add datetime to views to make dates cleaner for the user

--- drf_api/serializers.py
+++ drf_api/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from dj_rest_auth.serializers import UserDetailsSerializer
 from rest_framework import serializers
 
@@ -5,6 +6,14 @@ from rest_framework import serializers
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source="profile.id")
     profile_image = serializers.ReadOnlyField(source="profile.image.url")
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
 
     class Meta:
         fields = UserDetailsSerializer.Meta.fields + ("profile_id", "profile_image")
--- drf_api/settings.py
+++ drf_api/settings.py
@@ -33,8 +33,17 @@ REST_FRAMEWORK = {
             if os.getenv("DEVELOPMENT")
             else "dj_rest_auth.jwt_auth.JWTCookieAuthentication"
         )
-    ]
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    "DATETIME_FORMAT": "%d %b %Y",
 }
+
+if not os.getenv("DEVELOPMENT"):
+    REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = [
+        "rest_framework.renderers.JSONRenderer",
+    ]
+
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
