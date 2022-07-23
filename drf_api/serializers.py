2e69964535809c67bcd82d39601da2900fb23ec9
Tom Ainsworth <tomainsworth94@gmail.com>
2022-07-23 21:29:44 +0100

    Fix main serializer meta class to properly display fields

--- drf_api/serializers.py
+++ drf_api/serializers.py
@@ -6,5 +6,5 @@ class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source="profile.id")
     profile_image = serializers.ReadOnlyField(source="profile.image.url")
 
-    class Meta:
+    class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + ("profile_id", "profile_image")
--- drf_api/settings.py
+++ drf_api/settings.py
@@ -53,7 +53,7 @@ JWT_AUTH_REFRESH_COOKIE = "my-refresh-token"
 JWT_AUTH_SAMESITE = "None"
 
 REST_AUTH_SERIALIZERS = {
-    "USER_DETAILS_SERIALIZER": "drf_api.serializers.UserDetailsSerializer"
+    "USER_DETAILS_SERIALIZER": "drf_api.serializers.CurrentUserSerializer"
 }
 
 
